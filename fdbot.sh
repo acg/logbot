bf0c9bd41107843a034e43271f359e44429300f2
Alan Grow <agrow@scalar.(none)>
2009-06-09 13:51:29 -0400

    remove autojoin; join channels in response to invite

--- fdbot.sh
+++ fdbot.sh
@@ -1,32 +1,50 @@
 #!/bin/sh
 
 botname="fdbot"
-channel="&fdb"
 
 say() {
-  printf "$1\n"
+  echo "$1"
   echo 1>&2 "$1"
 }
 
+nextword() {
+  echo "${line%% *}"
+}
+
+rest() {
+  echo "${line#* }"
+}
+
 say "NICK $botname"
-say "USER $botname $botname * :IRC logbot for $channel"
+say "USER $botname $botname * :IRC logbot"
 
 while read line; do
   echo 1>&2 "$line"
+
   case "$line" in
     :*)
-      prefix="${line%% *}"
-      line="${line#:* }"
+      prefix=`nextword`
+      line=`rest`
       ;;
   esac
-  case "$line" in
-    PING" "*)
-      rest="${line##PING}"
-      say "PONG$rest"
+
+  command=`nextword`
+  line=`rest`
+
+  case "$command" in
+    PING)
+      say "PONG $line"
       ;;
-    254" "*)       # "N channels formed", part of welcome
-      say "JOIN $channel"
+    INVITE)
+      who=`nextword`
+      line=`rest`
+      channel=`nextword`
+      line=`rest`
+      if [ "$who" = "$botname" ]; then
+        say "JOIN $channel"
+      fi
       ;;
   esac
+
 done
 
